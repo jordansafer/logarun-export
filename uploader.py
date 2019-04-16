89246c766b306a7a68a5884fb813caa303b302f5
jordansafer <jsafer@andrew.cmu.edu>
2019-04-16 08:02:01 -0400

    uploader for logarun with note

--- uploader.py
+++ uploader.py
@@ -6,7 +6,21 @@ import csv
 import shutil
 import time
 import datetime as dt
-from datetime import datetime
+from datetime import datetime, timedelta
+import json
+
+def get_duration(ts):
+  t = datetime.strptime(ts, "%H:%M:%S")
+  td = timedelta(hours=t.hour, minutes=t.minute, seconds=t.second)
+  return int(td.total_seconds())
+
+def get_meters(dist, units):
+  if (units.startswith("Kilo")):
+    return dist * 1000
+  elif (units.startswith("Meter")):
+    return dist
+  else: # miles
+    return dist * 1609.344
 
 def main():
 
@@ -22,46 +36,50 @@ def main():
     client = Client()
 
     # You need to run the strava_local_client.py script - with your application's ID and secret - to generate the access token.
-    access_token = "442a2a9e4db1f7008fc96789e18c16e05875305d" # replace this with your token
+    access_token = "ebdaaf4293b002c782fd56c0b90720d3c605ae86" # replace this with your token
     client.access_token = access_token
     athlete = client.get_athlete()
     logger("Now authenticated for " + athlete.firstname + " " + athlete.lastname)
            
     # We open the cardioactivities CSV file and start reading through it
-    with open('logarun.csv') as csvfile:
-        activities = csv.reader(csvfile)
+    with open('logarun_export_20140601_20190415.json') as f:
+        days = json.load(f)
         activity_counter = 0
-        for row in activities:
-            if row[2] == 'time':
-                continue
+        for day in days:
             if activity_counter >= 500:
                 logger("Upload count at 500 - pausing uploads for 15 minutes to avoid rate-limit")
                 time.sleep(900)
                 activity_counter = 0
-            if row[0] not in log:
-                logger("Manually uploading " + row[0])
-                duration = int(float(row[2])) # time in seconds
-                dist = float(row[1])*1609.344 # convert miles to meters
-                starttime = row[0]
-                strava_activity_type = "Run"
+            note = day['note']
+            date = day['date']
+            title = day['title']
+            for act in day['activities']:
+                if (not act['type'].startswith('Run')):
+                    continue
+                logger("Manually uploading " + date)
+                duration = get_duration(act['Time']) # time in seconds
+                dist = get_meters(float(act['Distance']), act['Units'])
 
-
+                # extra
+                shoes = act['Shoes']
+           
                 try:
                     upload = client.create_activity(
-                        name = "logarun Run",
-                        start_date_local = starttime,
+                        name = title,
+                        start_date_local = date + "T00:00:00Z",
                         elapsed_time = duration,
                         distance = dist,
-                        description = "Shoe used: " + row[3],
-                        activity_type = strava_activity_type
+                        description = note + "\n Shoes: " + shoes,
+                        activity_type = "Run"
                     )
                     
-                    logger("Manually created " + row[0])
+                    logger("Manually created " + date)
                     activity_counter += 1
 
                 except ConnectionError as err:
                     logger("No Internet connection: {}".format(err))
                     exit(1)
+                note = "" # hack so note isn't repeated for doubles
 
         logger("Complete! Logged " + str(activity_counter) + " activities.")
 
